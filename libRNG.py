e5060c529448d1887a4e776fbf1acd97afa797f2
Andrebtk <anbertok25@gmail.com>
2023-07-06 14:27:16 +0200

    Fix number issue

---
+++ libRNG.py
@@ -0,0 +1,59 @@
+import sys
+from qiskit import QuantumCircuit
+from qiskit.providers.aer import AerSimulator
+from qiskit import Aer
+
+
+def maxCount(res, nQubit):
+    maxF = (nQubit)*"0"
+    for i in res:
+        if int(res[i]) > int(res[maxF]):
+            maxF = i            
+    return maxF
+
+
+def bin2Dec(bin):
+    return int(bin,2)
+
+
+def QuantRNG(maxInput):
+    n=0
+    for i in range(maxInput):
+        if 2**i < maxInput:
+            n+=1
+
+    maxQubitValue = 2**n
+    delta = maxQubitValue - maxInput
+
+    qc = QuantumCircuit(n)
+
+    for i in range(n):
+        qc.h(i)
+
+    qc.measure_all()
+
+    if len(sys.argv) > 1:
+        for i in sys.argv:
+            if i == '-circuit':
+                print(qc)
+
+    backend = Aer.get_backend('aer_simulator')
+    res = backend.run(qc).result().get_counts()
+
+    result = bin2Dec(maxCount(res, n))
+
+    if result > delta:
+        result-=delta
+
+    return result
+
+
+#Not used yet
+def XY(res):
+    xData = []
+    yData = []
+
+    for i in res:
+        yData.append(int(res[i]))
+        xData.append(int(i))
+    return (xData, yData)
---
+++ quantumRNG.py
@@ -0,0 +1,8 @@
+from libRNG import maxCount, bin2Dec, XY, QuantRNG
+
+
+maxInput = int(input("Generate a number between 0 and: "))
+
+print(QuantRNG(maxInput))
+
+
